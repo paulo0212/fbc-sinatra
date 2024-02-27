7732890edd1103ae6ab41f1a5e098b349031750e
paulo0212 <rikuo.iwasaki@gmail.com>
2024-02-27 18:09:49 +0900

    jsonファイルの読み書き処理を追加

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
+
+FILE_PATH = './data/memos.json'
 
 ['/', '/memos'].each do |path|
   get path do
@@ -15,6 +18,19 @@ get '/memos/new' do
 end
 
 post '/memos/create' do
-  # データを保存する処理を追加
+  memos = read_memos(FILE_PATH) || {}
+  max_id = memos.keys.map(&:to_i).max || 0
+  memos[max_id + 1] = params
+  save_memos(FILE_PATH, memos)
   redirect '/memos'
+end
+
+def read_memos(file_path)
+  return nil if File.zero?(file_path)
+
+  File.open(file_path, 'r') { |f| JSON.parse(f.read) }
+end
+
+def save_memos(file_path, memos)
+  File.open(file_path, 'w') { |f| JSON.dump(memos, f) }
 end
